28c56be21be71d779fcd250ed812dda3bbc614e0
NipsyKay <157756668+NipsyKay@users.noreply.github.com>
2024-07-09 01:58:40 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,19 +4,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "916909137213"
-global.ownername = "David Cyril"
-global.ytname = "David Cyril"
-global.socialm = "GitHub: DeeCeeXxx"
-global.location = "Africa, Nigeria, Lagos"
+global.ownernomer = "260776642930"
+global.ownername = "Nipsy-Hus-Tle"
+global.ytname = "Nipsy-Hus-Tle"
+global.socialm = "GitHub: NipsyKay"
+global.location = "Africa, Zambia, Lusaka"
 
 global.ownernumber = '2347043759577,2349066528353,254782572110'  //creator number
-global.ownername = 'David Cyril' //owner name
+global.ownername = 'Nipsy-Hus-Tle' //owner name
 global.botname = '🕷Venom-2🕷' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'David_Cyril\n\nContact: +2349066528353'
+global.author = 'Nipsy-Hus-Tle🥂'
 
 //console view/theme
 global.themeemoji = '🪀'
@@ -31,7 +31,7 @@ global.prefa = ['','!','.','#','&']
 //false=disable and true=enable
 global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
-global.autorecordtype = true //auto typing + recording
+global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
